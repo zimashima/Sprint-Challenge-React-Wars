b0d307605aab027d719b488e3d56b1d38d920d0e
Mashima <52222646+zimashima@users.noreply.github.com>
2019-11-08 11:31:34 -0500

    set parent fonts, added header, footer, character card components, and data for testing

---
+++ starwars/src/components/CharacterCard/CharacterCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardCharacter = `
+    background-color: orange;
+`
+
+export default function CharacerCard(){
+
+    return (
+        <div className="card_Character">
+            
+        </div>
+    )
+
+}
---
+++ starwars/src/components/HeaderFooter/HeaderFooter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Header = ()=>{
+    return (
+        <header>
+        <h1>STARWARS</h1>
+        </header>
+    )    
+}
+
+export const Footer = ()=>{
+    return (
+        <footer>
+        <p>May the Force Be with You</p>
+        </footer>
+    )
+}
